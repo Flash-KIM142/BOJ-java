a388ebb443ec926b822cde8c3665e45236b8846b
Flash-KIM142 <topqr123q@gmail.com>
2022-02-24 21:59:57 +0900

    가장 긴 감소하는 순열 ( DP )

--- src/boj11722.java
+++ src/boj11722.java
@@ -2,34 +2,31 @@ import java.util.*;
 import java.io.*;
 
 public class boj11722 {
-    static int n, ary[], dp[], answer=-1;
-
-    static void DP(){
-        dp[0] = 1;
-        for(int i=1; i<n; i++){
-            dp[i] = 1;
-            for(int j=0; j<i; j++){
-                if(ary[j]>ary[i] && dp[j]+1>=dp[i])
-                    dp[i] = dp[j] + 1;
-            }
-        }
-
-        for(int i=0; i<n; i++){
-            answer = Math.max(answer, dp[i]);
-        }
-    }
+    static int n;
+    static int[] input;
+    static int[] dp;
 
     public static void main(String args[]) throws IOException {
         BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk = new StringTokenizer(bfr.readLine());
         n = Integer.parseInt(stk.nextToken());
-        ary = new int[n+1];
-        dp = new int[n+1];
         stk = new StringTokenizer(bfr.readLine());
+
+        input = new int[n];
+        dp = new int[n];
+        for(int i=0; i<n; i++)
+            input[i] = Integer.parseInt(stk.nextToken());
+
+        int answer = 1;
         for(int i=0; i<n; i++){
-            ary[i] = Integer.parseInt(stk.nextToken());
+            dp[i] = 1;
+            for(int j=0; j<i; j++){
+                if(input[j]>input[i]){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+            answer = Math.max(answer, dp[i]);
         }
-        DP();
         System.out.println(answer);
     }
 }
